90447d33d39fb44d958d1815c02fd8765bc061f6
CodeX97 <thamindu.17@cse.mrt.ac.lk>
2019-09-26 15:24:10 +0530

    First Page Edited

--- lib/main.dart
+++ lib/main.dart
@@ -15,167 +15,70 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Course {
-  //TODO implement as needed
-}
-
 //-----------------------------------First Screen Implementation-----------------------------------
 class StartQ extends StatefulWidget {
   StartQState createState() => StartQState();
 }
 
 class StartQState extends State<StartQ> {
+  final _departments = <String>[
+    'Chemical and Process Engineering',
+    'Civil Engineering',
+    'Computer Science and Engineering',
+    'Earth Resources and Engineering',
+    'Electrical Engineering',
+    'Electronic and Telecommunication Engineering',
+    'Material Science and Engineering',
+    'Mechanical Engineering',
+    'Textile and Clothing Technology',
+    'Transport and Logistics Management'
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('GPA Calculator  |  Uom E-Fac'),
       ),
-      body: _buildIsMPR(),
+      body: _buildSelectDept(),
       backgroundColor: Color.fromRGBO(3, 48, 22, 1),
     );
   }
 
-  Widget _buildIsMPR() {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(
-            'Select your stream',
-            style: TextStyle(color: Colors.white, fontSize: 28),
+  Widget _buildSelectDept() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: (_departments.length * 2) + 1,
+        itemBuilder: (context, i) {
+          if (i == 0)
+            return Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 40.0),
+                child: Text(
+                  'Select Your Department',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ));
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2;
+          return _buildRow(_departments[index - 1]);
+        });
+  }
+
+  Widget _buildRow(department) {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
+        child: ButtonTheme(
+          minWidth: 350.0,
+          height: 50.0,
+          child: RaisedButton(
+            onPressed: () => navigateToRoute(department),
+            textColor: Colors.white,
+            color: Color.fromRGBO(1, 24, 11, 1),
+            shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(10.0)),
+            child: Text(department,
+                textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
           ),
-          const SizedBox(height: 70),
-          ButtonTheme(
-            minWidth: 200.0,
-            height: 100.0,
-            child: RaisedButton(
-              onPressed: _pushToMPR,
-              textColor: Colors.white,
-              color: Color.fromRGBO(1, 24, 11, 1),
-              padding: const EdgeInsets.all(0.0),
-              shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(10.0)),
-              child: const Text('MPR', style: TextStyle(fontSize: 28)),
-            ),
-          ),
-          const SizedBox(height: 50),
-          ButtonTheme(
-            minWidth: 200.0,
-            height: 100.0,
-            child: RaisedButton(
-              onPressed: _pushToOther,
-              textColor: Colors.white,
-              color: Color.fromRGBO(1, 24, 11, 1),
-              padding: const EdgeInsets.all(0.0),
-              shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(10.0)),
-              child: const Text('EM, TM, TLM', style: TextStyle(fontSize: 28)),
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 
-  void _pushToMPR() {
-    Navigator.push(
-      context,
-      new MaterialPageRoute(builder: (context) => new MPRFirstScreen()),
-    );
-  }
-
-  void _pushToOther() {
-    Navigator.push(
-      context,
-      new MaterialPageRoute(builder: (context) => new OtherFirstScreen()),
-    );
-  }
-}
-//-----------------------------------End of First Screen Implementation--------------------------
-
-class LabeledRadio extends StatelessWidget {
-  const LabeledRadio({
-    this.label,
-    this.padding,
-    this.groupValue,
-    this.value,
-    this.onChanged,
-  });
-
-  final String label;
-  final EdgeInsets padding;
-  final bool groupValue;
-  final bool value;
-  final Function onChanged;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        if (value != groupValue) onChanged(value);
-      },
-      child: Padding(
-        padding: padding,
-        child: Row(
-          children: <Widget>[
-            Radio<bool>(
-              groupValue: groupValue,
-              value: value,
-              onChanged: (bool newValue) {
-                onChanged(newValue);
-              },
-            ),
-            Text(label),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-//-----------------------------------MPR First Screen Implementation-----------------------------
-class MPRFirstScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text('MPR'),
-      ),
-      body: _mprSelect(),
-      backgroundColor: Color.fromRGBO(3, 48, 22, 1),
-    );
-  }
-
-  Widget _mprSelect() {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(
-            "Semester you're currently enrolled in",
-            style: TextStyle(color: Colors.white, fontSize: 23),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-//-----------------------------------End of MPR First Screen Implementation------------------------
-
-//-----------------------------------Other First Screen Implementation-----------------------------
-class OtherFirstScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text('EM | TM | TLM'),
-      ),
-      body: _otherSelect(),
-    );
-  }
-
-  Widget _otherSelect() {
-    return null;
-  }
+  void navigateToRoute(String department) {}
 }
